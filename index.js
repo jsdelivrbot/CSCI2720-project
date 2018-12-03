5756587cba39701ca3ce4fd6b3305053ca814110
leecat2001a <leecat2001@yahoo.com.hk>
2018-12-03 22:31:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var app = express();
+
+app.get('/',function(req,res){
+	res.send('Hello Wold!');
+})
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('login.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
