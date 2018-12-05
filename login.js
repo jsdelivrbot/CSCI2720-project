3e818a37cd23acebceeda79e0334a9036a47ac15
leecat2001a <leecat2001@yahoo.com.hk>
2018-12-05 15:54:26 +0800

    Update login.js

--- login.js
+++ login.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 
     $('#loginButton').click(function(){
     	$.get("/user.html",function(data){
+    		window.history.pushState(null, null, "/user");
     		$("html").html(data);
-    		 window.history.pushState(null, null, "/user");
     	});
     });
 });
