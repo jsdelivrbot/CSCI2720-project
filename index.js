d6d88410ef9d07d4e532c1d5dd2f56aaf5a3cb51
leecat2001a <leecat2001@yahoo.com.hk>
2018-12-06 21:23:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ var UserSchema = new mongoose.Schema({
     password: {type: String, required: true}
 });
 
+//The event schema is just temp, will change to real data later
+var EventSchema = new mongoose.Schema({
+	eventName: {type: String, required: true},
+	date: {type: String, required: true},
+	location: {type: String, required: true}
+});
+
 var User = mongoose.model('User', UserSchema);
+var Event = mongoose.model('Event', EventSchema);
 
 //Get Post Delete
 
@@ -30,11 +38,25 @@ app.post('/test',function(req,res){
 		loginId: String(req.body['loginId']),
 		password: String(bcrypt.hashSync(req.body['password']))
 	});
+	var e1 = new Event({
+		eventName: "test",
+		date: "9-9-2018",
+		location: "Hong Kong"
+	});
+	e1.save();
+
+	var e2 = new Event({
+		eventName: "test2",
+		date: "2-9-2018",
+		location: "Taiwan"
+	})
+	e2.save();
 	u.save(function(err){
 		if(err)
 			res.send(err)
 		res.send("Account created! Login Id: "+req.body['loginId']);
 	});
+
 });
 
 app.post('/user',function(req,res){
